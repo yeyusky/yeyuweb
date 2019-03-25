33985d8a7f97babaea4360f4c25bdff2e2432459
web-yeyu <327853133@qq.com>
2019-03-25 10:41:53 +0800

    UC首页

---
+++ uspage/js/index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    var openpic = document.getElementById("pic");
+    var obtn1 = document.getElementById("btn1");
+    var obtn2 = document.getElementById("btn2");
+
+
+    obtn1.onclick = function () {
+
+        openpic.style.backgroundImage = "url(../uspage/images/bg1.png)";
+        obtn1.style.backgroundImage = "url(../uspage/images/scrolldot1.png)";
+        obtn2.style.backgroundImage = "url(../uspage/images/scrolldot2.png)";
+    }
+    obtn2.onclick = function () {
+
+        openpic.style.backgroundImage = "url(../uspage/images/bg2.png)";
+        obtn2.style.backgroundImage = "url(../uspage/images/scrolldot1.png)";
+        obtn1.style.backgroundImage = "url(../uspage/images/scrolldot2.png)";
+    }
+}
